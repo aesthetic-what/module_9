2df3bce2467a6df3c612cbe8c65efdad79b1d53c
aesthetic-what <ladnoladno477@gmail.com>
2024-10-02 23:49:07 +0500

    Add files via upload

--- module_9_4.py
+++ module_9_4.py
@@ -1,10 +1,9 @@
 import random
 
-
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 
-first_res = list(map(lambda x, y: x.split() == y.split(), first, second))
+first_res = list(map(lambda x, y: x == y, first, second))
 print(first_res)
 
 
@@ -12,12 +11,12 @@ def get_advanced_writer(file_name):
 
     def write_everything(*data_set):
         with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(str(data_set))
+            file.write('\n'.join(map(str, data_set)))
     return write_everything
 
 
 write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'], '1234')
 
 
 class MysticBall():
